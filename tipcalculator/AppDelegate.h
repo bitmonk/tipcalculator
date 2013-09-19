ef8f31710f5a12854b76027932b2800903263b20
Justin Alan Ryan <serial.rockstar@gmail.com>
2013-09-19 15:04:47 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Justin A Ryan on 9/19/13.
+//  Copyright (c) 2013 Justin A Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
