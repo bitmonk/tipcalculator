547bc91c5169deadcb88e9e25252699b71580adb
Justin Alan Ryan <serial.rockstar@gmail.com>
2013-09-19 16:19:04 -0700

    Basic implementation of Tip Calculator following http://vimeo.com/74764846

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Justin A Ryan on 9/19/13.
+//  Copyright (c) 2013 Justin A Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
